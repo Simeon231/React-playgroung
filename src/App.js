be27516cc170b618367e81729fcf12b59e02b1f7
Simeon Kirilov <Simeon231@users.noreply.github.com>
2021-03-21 13:34:17 +0200

    Add nav

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
 import './App.css';
 import Heading from './components/Heading';
 import Ul from './components/Ul';
-import Increment from './components/Increment';
+import Increment from './components/Main/Increment';
+import Nav from './components/Header/Nav';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const lorems = [
-  'Anim mollit fugiat enim cupidatat et proident ex non deserunt labore aute et.', 
-  'Ad velit irure incididunt excepteur veniam est.',
-  'Irure officia commodo aliqua consectetur dolor deserunt enim elit cupidatat culpa pariatur.'];
+    {
+        id: 1,
+        value: 'Anim mollit fugiat enim cupidatat et proident ex non deserunt labore aute et.',
+    },
+    {
+        id: 2,
+        value: 'Ad velit irure incididunt excepteur veniam est.',
+    },
+    {
+        id: 3,
+        value: 'Irure officia commodo aliqua consectetur dolor deserunt enim elit cupidatat culpa pariatur.'
+    }];
 
 function App() {
-  return (
-    <div className="container mt-5">
-      <div className="d-flex justify-content-center">
-        <Increment />
-      </div>
-    <Heading >
-      Hello world
-    </Heading>
+    return (
+        <div>
+            <Nav />
+            <div className="container mt-5">
+                <div className="d-flex justify-content-center">
+                    <Increment />
+                </div>
+                <Heading >
+                    Hello world
+      </Heading>
 
-    <Ul elements={lorems} clickHandler={() => AddLi(lorems[0])} ></Ul>
-    </div>
-  );
+                <Ul elements={lorems} clickHandler={() => AddLi(lorems[0].value)} ></Ul>
+            </div>
+        </div>
+    );
 }
 
-function AddLi(li){
+function AddLi(li) {
     console.log(li);
 }
 
